7e966e13c7360183f658b9f399b85c3e5d6eb44f
orzymandias <ganwancheng@gmail.com>
2020-06-23 00:47:31 +0800

    added model validation before search bar is returned

---
+++ client/src/components/AppBar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const AppBar = (props) => {
+  return (
+    <>
+      <div id="appbar-container">{/* <p>HI</> */}</div>
+    </>
+  );
+};
--- client/src/pages/SearchResults/SearchResults.js
+++ client/src/pages/SearchResults/SearchResults.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { EMOTION_CLASSES } from '../../utils/Constants';
 import ResultsPanel from '../../components/ResultsPanel/ResultsPanel';
+// import { TRACKS_SAMPLE } from '../../utils/Mocks';
 import './SearchResults.css';
 
 const SearchResults = (props) => {
@@ -20,7 +21,7 @@ const SearchResults = (props) => {
           </span>{' '}
         </p>
       </div>
-
+      {/* <ResultsPanel tracks={TRACKS_SAMPLE} /> */}
       {props.recommendation.tracks ? (
         <ResultsPanel tracks={props.recommendation.tracks} />
       ) : null}
