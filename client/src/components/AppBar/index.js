ee4469f55da3e0df75bf46973ef64252cb14f401
orzymandias <ganwancheng@gmail.com>
2020-07-21 16:43:08 +0800

    added logout feature

--- client/src/components/AppBar/index.js
+++ client/src/components/AppBar/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useContext } from "react";
-import { withRouter, useLocation } from "react-router-dom";
-import { userContext } from "../../context/UserProvider";
-import querySearch from "stringquery";
-import "./index.css";
+import React, { useEffect, useContext } from 'react';
+import { withRouter } from 'react-router-dom';
+import { userContext } from '../../context/UserProvider';
+import querySearch from 'stringquery';
+import './index.css';
 
 const SPOTIFY_USER_AUTH = `${process.env.REACT_APP_HOST}/auth/spotify`;
 
@@ -14,14 +14,14 @@ export const AppBar = withRouter((props) => {
     // Update user variables and accessTOken
     if (props.location.search) {
       let query = querySearch(props.location.search);
-      dispatch({ type: "SET_USER", payload: query.user });
-      dispatch({ type: "SET_TOKEN", payload: query.token });
-      sessionStorage.setItem("userLoggedIn", true);
+      dispatch({ type: 'SET_USER', payload: query.user });
+      dispatch({ type: 'SET_TOKEN', payload: query.token });
+      sessionStorage.setItem('userLoggedIn', true);
 
       window.history.replaceState(null, null, `${window.location.origin}/`);
     }
     // CASE 2: if user already logged in based on current session but accessToken missing
-    else if (sessionStorage.getItem("userLoggedIn") && !userState.accessToken) {
+    else if (sessionStorage.getItem('userLoggedIn') && !userState.accessToken) {
       window.location.href = SPOTIFY_USER_AUTH;
     }
 
@@ -29,12 +29,25 @@ export const AppBar = withRouter((props) => {
     else {
     }
   }, []);
+
+  const onClickLogin = (e) => {
+    return isLoggedIn()
+      ? handleLogout()
+      : (window.location = SPOTIFY_USER_AUTH);
+  };
+  const handleLogout = () => {
+    dispatch({ type: 'LOG_OUT', payload: '' });
+  };
+
+  const isLoggedIn = () => {
+    return userState.userObject ? true : false;
+  };
   return (
     <>
       <div id="appbar-container">
-        <form action={SPOTIFY_USER_AUTH}>
-          <button id="account-button">Sign in with Spotify</button>
-        </form>
+        <button id="account-button" onClick={onClickLogin}>
+          {isLoggedIn() ? 'Logout of spotify' : 'Sign in with Spotify'}
+        </button>
       </div>
     </>
   );
