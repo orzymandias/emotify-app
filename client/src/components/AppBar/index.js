21e484cfa3a5195f16273380c8ca005ba279e210
Shawn Ng <shawnngyongkwang@gmail.com>
2020-07-22 23:46:37 +0800

    Update favicon and website title

--- client/src/components/AppBar/index.js
+++ client/src/components/AppBar/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useContext } from 'react';
-import { withRouter } from 'react-router-dom';
-import { userContext } from '../../context/UserProvider';
-import querySearch from 'stringquery';
-import './index.css';
+import React, { useEffect, useContext } from "react";
+import { withRouter } from "react-router-dom";
+import { userContext } from "../../context/UserProvider";
+import querySearch from "stringquery";
+import "./index.css";
 
 const SPOTIFY_USER_AUTH = `${process.env.REACT_APP_HOST}/auth/spotify`;
 
@@ -14,14 +14,14 @@ export const AppBar = withRouter((props) => {
     // Update user variables and accessTOken
     if (props.location.search) {
       let query = querySearch(props.location.search);
-      dispatch({ type: 'SET_USER', payload: query.user });
-      dispatch({ type: 'SET_TOKEN', payload: query.token });
-      sessionStorage.setItem('userLoggedIn', true);
+      dispatch({ type: "SET_USER", payload: query.user });
+      dispatch({ type: "SET_TOKEN", payload: query.token });
+      sessionStorage.setItem("userLoggedIn", true);
 
       window.history.replaceState(null, null, `${window.location.origin}/`);
     }
     // CASE 2: if user already logged in based on current session but accessToken missing
-    else if (sessionStorage.getItem('userLoggedIn') && !userState.accessToken) {
+    else if (sessionStorage.getItem("userLoggedIn") && !userState.accessToken) {
       window.location.href = SPOTIFY_USER_AUTH;
     }
 
@@ -36,7 +36,7 @@ export const AppBar = withRouter((props) => {
       : (window.location = SPOTIFY_USER_AUTH);
   };
   const handleLogout = () => {
-    dispatch({ type: 'LOG_OUT', payload: '' });
+    dispatch({ type: "LOG_OUT", payload: "" });
   };
 
   const isLoggedIn = () => {
@@ -46,7 +46,7 @@ export const AppBar = withRouter((props) => {
     <>
       <div id="appbar-container">
         <button id="account-button" onClick={onClickLogin}>
-          {isLoggedIn() ? 'Logout of spotify' : 'Sign in with Spotify'}
+          {isLoggedIn() ? "Log out of Spotify" : "Sign in with Spotify"}
         </button>
       </div>
     </>
